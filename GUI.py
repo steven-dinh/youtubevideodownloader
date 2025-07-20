2f09ff7e8a5a222d1dcbadc0481402a5a36b3f66
Steven <stevend3373@gmail.com>
2025-07-20 16:35:46 -0400

    create url part of gui

--- GUI.py
+++ GUI.py
@@ -1,16 +1,30 @@
 from pytube import YouTube
 import tkinter as tk
 
+#functions
+#def
+
+def getEntry(entryName, updateLabel):
+    entry = entryName.get()
+    updateLabel.config(text=f"Current url: {entry}")
+
+
 root = tk.Tk()
+root.geometry("350x400")
 root.title("Youtube Video Downloader")
 
-label = tk.Label(root, text="Hello, Tkinter!")
-label.pack()
+#url part of gui
+enterUrlLabel = tk.Label(root, text="Enter url")
+enterUrlLabel.grid(row=0, column=0)
 
-urlEntry = tk.Entry(root)
-urlEntry.pack()
+urlEntry = tk.Entry(root, width=40)
+urlEntry.grid(row=1, column=0, padx=10, pady=5)
 
-button = tk.Button(root, text="Close", command=root.destroy)
-button.pack(expand=True,fill='x')
+urlLabel = tk.Label(root, text="Current url: ")
+urlLabel.grid(row=2, column=0, padx=(10, 0), sticky="w")
+
+entryButton = tk.Button(root, text="Enter", command=lambda: getEntry(urlEntry, urlLabel))
+entryButton.grid(row=1, column=1)
+
 
 root.mainloop()
